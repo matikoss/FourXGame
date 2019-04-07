b050510f9e83bd9d6960318d7543e7b475ef28c1
matikoss <m.kossakowski0@gmail.com>
2019-04-07 15:06:05 +0200

    Git setup

---
+++ desktop/src/com/mygdx/fourxgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.fourxgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.fourxgame.FourXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FourXGame(), config);
+	}
+}
